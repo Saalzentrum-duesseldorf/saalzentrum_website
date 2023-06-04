243bbb62f2bea89e8f21450288bd852fe32f6f8c
jkoch1220 <jtatwcook@gmail.com>
2023-06-04 20:09:18 +0200

    Fix build

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import BurgerMenu from "./compontents/menu/BurgerMenu.tsx";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Calendar } from "./compontents/calendar/Calendar.tsx";
 import StartPage from "./compontents/startPage/StartPage.tsx";
-import  IssueCollector from "./compontents/jiraIssueCollector/IssueCollector.tsx";
+// import  IssueCollector from "./compontents/jiraIssueCollector/IssueCollector.tsx";
 
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<StartPage/>} />
         <Route path="/calendar" element={<Calendar/>} />
-        <Route path="/tickets" element={<IssueCollector/>} />
+        {/*<Route path="/tickets" element={<IssueCollector/>} />*/}
       </Routes>
       <Footer/>
     </Router>
--- src/compontents/startPage/StartPage.tsx
+++ src/compontents/startPage/StartPage.tsx
@@ -1,11 +1,24 @@
 import Header from "../header/Header.tsx";
 import FlipCard from "../flipCard/FlipCard.tsx";
+import "./StartPage.scss"
 
 function App() {
-  return (<div>
+  return (<div className={"startpage"}>
     <Header/>
     <div className={'text-center'}>
-      <FlipCard/>
+      <FlipCard />
+
+      <div className={"starterText"}>
+        <div className={"starterTextHeader"}>
+        Herzlich willkommen auf der Website für das Saalzentrum in der
+        Langenberger Straße 5 in Düsseldorf. </div>
+        Hier findest du organisatorische
+        Informationen und Übersichten. Außerdem gibt es die Möglichkeit ein Ticket zu eröffnen.
+        Dies ist insbesondere für Termine, die in den Kalender eingetragen werden sollen
+        und Meldungen von Defekten oder notwendigen Instandhaltungsarbeiten im
+        Saalkomplex vorgesehen. Auch bei Fragen und Anliegen zu dieser Website
+        kann ein Ticket eröffnet werden.
+      </div>
     </div>
   </div>)
 }
